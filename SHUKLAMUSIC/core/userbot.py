5ceb2c34960580b972d4eb31756d7cead5177c69
𝙏𝙊𝙓𝙄𝘾-𝙓𝘿 <149950962+TOXICOP638@users.noreply.github.com>
2025-01-20 13:40:22 +0530

    Update userbot.py

--- SHUKLAMUSIC/core/userbot.py
+++ SHUKLAMUSIC/core/userbot.py
@@ -51,8 +51,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("MBV_CHATS")
-                await self.one.join_chat("MBV_NETWORK")
+                await self.one.join_chat("TSERIESSUPPORTCHAT")
+                await self.one.join_chat("TSERIESUPDATESS")
             except:
                 pass
             assistants.append(1)
